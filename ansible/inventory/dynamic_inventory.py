4dd87d02e3d45df741745240a468f3cac9cbcd29
SteveNdegwa <stevencallistus19@gmail.com>
2026-01-17 23:22:30 +0300

    updates

--- ansible/inventory/dynamic_inventory.py
+++ ansible/inventory/dynamic_inventory.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python3
-import yaml
-import json
 import os
+import json
+import yaml
 
-servers_dir = "data/servers"
+BASE_DIR = os.path.dirname(__file__)
+servers_dir = os.path.abspath(os.path.join(BASE_DIR, "../data/servers"))
+
 inventory = {"_meta": {"hostvars": {}}}
-
-all_group = {"hosts": [], "children": []}
-inventory["all"] = all_group
+inventory["all"] = {"hosts": [], "children": []}
 
 for filename in os.listdir(servers_dir):
     if filename.endswith(".yml"):
         path = os.path.join(servers_dir, filename)
         with open(path) as f:
             data = yaml.safe_load(f)
-            name = data["server_name"]
-            all_group["hosts"].append(name)
-            inventory["_meta"]["hostvars"][name] = {
-                "ansible_host": data.get("public_ip", name),
-                "ansible_user": data["ssh_user"],
-            }
+
+        name = data["server_name"]
+        inventory["all"]["hosts"].append(name)
+        inventory["_meta"]["hostvars"][name] = {
+            "ansible_host": data.get("public_ip", name),
+            "ansible_user": data["ssh_user"],
+        }
 
 print(json.dumps(inventory))
