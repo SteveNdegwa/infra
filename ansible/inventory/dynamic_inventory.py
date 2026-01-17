ca1cb977b0e2baf68629585f975c13364f6da284
SteveNdegwa <stevencallistus19@gmail.com>
2026-01-17 22:20:19 +0300

    initial commit

---
+++ ansible/inventory/dynamic_inventory.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import yaml
+import json
+import os
+
+servers_dir = "data/servers"
+inventory = {"_meta": {"hostvars": {}}}
+
+all_group = {"hosts": [], "children": []}
+inventory["all"] = all_group
+
+for filename in os.listdir(servers_dir):
+    if filename.endswith(".yml"):
+        path = os.path.join(servers_dir, filename)
+        with open(path) as f:
+            data = yaml.safe_load(f)
+            name = data["server_name"]
+            all_group["hosts"].append(name)
+            inventory["_meta"]["hostvars"][name] = {
+                "ansible_host": data.get("public_ip", name),  # or use Terraform output
+                "ansible_user": data["ssh_user"],
+            }
+
+print(json.dumps(inventory))
